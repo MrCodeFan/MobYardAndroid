12ad9b1e310febf2389accf0c5e357d3ddb3535c
Damir <latypovdamir3@gmail.com>
2020-12-10 13:09:42 +0300

    adding MainYardActivity

---
+++ app/src/main/java/com/example/mobyardandroid/yard/MainYardActivity.java
@@ -0,0 +1,19 @@
+package com.example.mobyardandroid.yard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import com.example.mobyardandroid.R;
+
+public class MainYardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_yard);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+    }
+}
