e3ac327ad72684823901a57e2c71be0bdcfa9bc3
Marat Ash <mart.acc.mail@mail.ru>
2020-12-11 00:48:15 +0300

    Navigation Menu was added

--- app/src/main/java/com/example/mobyardandroid/user/UserDashboardActivity.java
+++ app/src/main/java/com/example/mobyardandroid/user/UserDashboardActivity.java
@@ -1,27 +1,41 @@
 package com.example.mobyardandroid.user;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.example.mobyardandroid.R;
 import com.example.mobyardandroid.auth.StartActivity;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 
-public class UserDashboardActivity extends AppCompatActivity {
+public class UserDashboardActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
+    // User`s data
     SharedPreferences YardsPrefs, PersPrefs;
     String email, userId, username, lastName, firstName;
 
     RecyclerView recyclerYards;
     RecyclerView.Adapter adapter;
+
+    // Drawer Menu
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ImageView menuIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,10 +60,45 @@ public class UserDashboardActivity extends AppCompatActivity {
 
         // Hooks
         recyclerYards = findViewById(R.id.recycler_yards);
+        menuIcon = findViewById(R.id.menu_icon);
+
+        // Menu Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        navigationDrawer();
+
 
         recyclerYards();
     }
 
+    private void navigationDrawer() {
+
+        // Navigation Drawer
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener( this );
+        navigationView.setCheckedItem(R.id.nav_home);
+
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START)){
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    drawerLayout.openDrawer( GravityCompat.START );
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)){
+            drawerLayout.closeDrawer( GravityCompat.START );
+        } else {
+            super.onBackPressed();
+        }
+    }
 
     protected void onStart() {
         super.onStart();
@@ -85,11 +134,15 @@ public class UserDashboardActivity extends AppCompatActivity {
             );
         }
 
-
-
         adapter = new YardsHomeAdapter(homeAdapters);
 
         recyclerYards.setAdapter(adapter);
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        return true;
     }
 }
