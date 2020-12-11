9036d693171b1d1dc1a51fc4edef666a68e54c15
Marat Ash <mart.acc.mail@mail.ru>
2020-12-11 15:34:49 +0300

    Some changes

--- app/src/main/java/com/example/mobyardandroid/utils/RandomString.java
+++ app/src/main/java/com/example/mobyardandroid/utils/RandomString.java
@@ -1,23 +1,15 @@
 package com.example.mobyardandroid.utils;
 
-import java.util.Date;
 import java.util.Random;
 
 public class RandomString {
 
-    private Random rand;
-    private Date time;
     private String output;
     private char[] alphabet =  {
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
     };
-
-    public RandomString() {
-        rand = new Random(  );
-    }
-
 
     public String gen(int num){
         output = "";
@@ -35,8 +27,8 @@ public class RandomString {
 
     public char genSymbol() {
         int endNum = alphabet.length;
-        int pos = (int) (Math.random() * endNum);
-        
+        int pos = (int) ( Math.random() * endNum );
+
         return alphabet[pos];
     }
 
